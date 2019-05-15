954a85dccea199a7d2cf4364cb87bb542816f2e9
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-05-15 21:52:34 +0800

    using start_urls as default

--- py_scrapy_demo/spiders/example.py
+++ py_scrapy_demo/spiders/example.py
@@ -3,14 +3,10 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "example"
-
-    def start_requests(self):
-        urls = [
+    start_urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            'http://quotes.toscrape.com/page/2/'
+    ]
 
     def parse(self, response):
         page = response.url.split("/")[-2]
