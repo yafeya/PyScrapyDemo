3e8f52501a00a33e9d3a5d82051d838069a327c9
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-05-15 21:49:40 +0800

    add first scrapy demo

--- py_scrapy_demo/spiders/example.py
+++ py_scrapy_demo/spiders/example.py
@@ -1,11 +1,20 @@
-# -*- coding: utf-8 -*-
 import scrapy
 
 
-class ExampleSpider(scrapy.Spider):
-    name = 'example'
-    allowed_domains = ['www.baidu.com']
-    start_urls = ['http://www.baidu.com/']
+class QuotesSpider(scrapy.Spider):
+    name = "example"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
